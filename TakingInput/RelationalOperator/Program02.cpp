fe1bb97be9d19180370e14291be2013c9bcf3be9
Prakash823517 <prakash765482@gmail.com>
2026-02-15 00:57:22 +0530

    Add files via upload

---
+++ TakingInput/RelationalOperator/Program02.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool p = false;
+    bool q = false;
+    bool r = true;
+    cout<<(p==q==r)<<endl;
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    cout<<(a==b==c);
+}
---
+++ TakingInput/SumOfTwoNumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1;
+    cout<<"Enter first number : ";
+    cin>>num1;
+    int num2;
+    cout<<"Enter second number : ";
+    cin>>num2;
+     int sum = num1 + num2;
+     cout<<"The sum of the numbers is : ";
+     cout<<sum;
+}
